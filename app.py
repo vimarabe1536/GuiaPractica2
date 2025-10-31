1164eafe6612226b28a9b428f8a689814ff36fda
vimarabe1536 <vm.ramirez220@uniandes.edu.co>
2025-10-31 17:05:47 -0500

    actualización funcion load_data

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
+    
     # To do: Completar la función
     data = pd.read_csv("datos_energia.csv")
     data['time'] = pd.to_datetime(data['time'])
